9e6d0bb2cc66e95603c57f55561068c5d27f629e
Maq21-p <maqsood.alikhan2@gmail.com>
2024-08-23 06:42:07 +0530

    changed redirect deleted social card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from "@docusaurus/router";
+import config from "@site/docusaurus.config";
 
 export default function Home(): JSX.Element {
-  return <Redirect to="/docs/intro" />;
+  return <Redirect to={config.baseUrl + "/docs/intro"} />;
 }
